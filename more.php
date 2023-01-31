658522235ad6632b7cf345b7dd29395104d37180
Mudassir Shahid <mudassirshahid84@gmail.com>
2023-01-31 18:48:20 +0500

    creating form

--- more.php
+++ more.php
@@ -13,7 +13,7 @@
   </head>
   <body>
     <h1 class="text-center">Learn About PHP</h1>
-    <div class="container text-center mt-5">
+    <div class="container-fkuid text-center mt-5">
     <p>Can I Go?</p>
     <?php
     $age = 20;
@@ -23,7 +23,102 @@
     else {
         echo "No You're Underage";
     }
+
+    echo "<br>";
+    echo "<br>";
+    echo "<h1>Arrays</h1>";
+    echo "<br>";
+    $languages = array("Python", "Javascript", "PHP", "NodeJS");
+    echo "<br>";
+    echo $languages[0];
+    echo "<br>";
+    echo $languages[1];
+    echo "<br>";
+    echo count($languages);
+
+    //LOOPS IN PHP 
+
+    echo "<br>";
+    echo "<br>";
+    echo "<h1>Loops</h1>";
+    echo "<br>";
+    $a = 0;
+    while ($a <= 10) {
+      echo "<br>The value of a from the while loop is: ";
+      echo $a;
+      $a++;
+    }
+    echo "<br>";
+    echo "<br>";
+
+
+    //ITERATING ARRAYS IN PHP USING WHILE LOOPS IN PHP 
+    $a = 0;
+    while ($a < count($languages)) {
+      echo "<br>The value of language is: ";
+      echo $languages[$a];
+      $a++;
+    }
+    echo "<br>";
+    echo "<br>";
+    
+    //DO WHILE LOOP    DIFFERENCE B/W WHILE AND DO WHILE IS IN DO WHILE FIRST VALUE EXECUTE THEN CONDITION CHECK IN WHILE LOOP  
+    $a = 0;
+    do {
+      echo "<br>The value of a from the do while loop is: ";
+      echo $a;
+      $a++;
+    } while ($a < 10);
+
+    echo "<br>";
+    echo "<br>";
+
+
+    //FOR LOOP 
+    for ($a=0; $a < 10; $a++) { 
+      echo "<br>The value of a from the for loop is: ";
+      echo $a;
+    }
+    echo "<br>";
+    echo "<br>";
+
+
+    //FOREACH LOOP 
+    foreach ($languages as $value) { 
+      echo "<br>The value of language from the foreach loop is: ";
+      echo $value;
+    }
+
+
+    //FUNCTIONS
+    function printNumber($number){
+      echo "<br>Your number is ";
+      echo $number;
+    }
+    printNumber(50);
+
+
+    echo "<br>";
+    echo "<br>";
+
+    //STRINGS
+    echo "<h1>Strings</h1>";
+    echo "<br>";
+    echo "<br>";
+    $str = "String Value";
+    $length = strlen($str);
+    echo "The length of this string is ".$length. " and this is the string concatenation";
+    echo "<br>";
+    echo "The word of this string is ". str_word_count($str). " and this is the string concatenation";
+    echo "<br>";
+    echo "The reverse of this string is ". strrev($str);
+    echo "<br>";
+    echo "The search of this string is ". strpos($str, 'ing');
+    echo "<br>";
+    echo "The replace string is ". str_replace('ing', '', $str);
     ?>
+
+    
     </div>
 
     <!-- Optional JavaScript -->
